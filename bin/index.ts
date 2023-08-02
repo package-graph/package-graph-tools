ba8f564e4563ff143da6f8fd50c28a02879252a0
jinzita-lx <xin15838645371@163.com>
2023-08-03 03:14:18 +0800

    chore: 🚀 配置rollup打包工具

--- bin/index.ts
+++ bin/index.ts
@@ -1,2 +1,24 @@
+import chalk from "chalk";
+import { Command } from "commander";
 
+import path from "path";
+import { analyze } from "../src";
+const program = new Command();
+program
+  .name("graph-cli")
+  .version(`v1.0.0`, '-v, --version', '查看版本号')
+  .description("字节青训营第六期大项目✨")
+  .command('analyze').description('分析依赖')
+  .option('-d, --tree [tree]', '设置深度分析依赖')
+  .option('-j, --json [file-path]', '输出json文件的目录')
+  .action(({depth, json}) => {
+  if(json){
+    // output json
+    console.log(json)
+  } else {
+    // analyze
+    analyze(depth)
+  }
+})
 
+program.parse(process.argv);
---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+/** 将json转换为ES6模块 */
+const json = require("@rollup/plugin-json");
+/** rollup解析及编译TS插件 */
+const typescript = require("@rollup/plugin-typescript");
+/** 解析代码中依赖的node_modules */
+const resolve = require("@rollup/plugin-node-resolve");
+/** 将 CommonJS 模块转换为 ES6 的 Rollup 插件 */
+const commonjs = require("@rollup/plugin-commonjs");
+/** rollup文件夹清除插件 */
+const { cleandir } = require("rollup-plugin-cleandir");
+
+module.exports = {
+  input: "./src/index.ts",
+  output: {
+    dir: "./lib",
+    format: "cjs",
+  },
+  plugins: [
+    cleandir("./lib"),
+    json(),
+    typescript({
+      module: "esnext",
+      exclude: ["./node_modules/**"],
+    }),
+    resolve.default({
+      extensions: [".js", ".ts", ".json"],
+      modulesOnly: true,
+      preferredBuiltins: false,
+    }),
+    commonjs({ extensions: [".js", ".ts", ".json"] }),
+  ],
+};
